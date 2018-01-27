4c31c5b0710b83cd17827a80bcf740c4a4f3be9f
ryk248 <32120690+ryk248@users.noreply.github.com>
2018-01-27 16:25:41 -0500

    Delete Project3.R

--- Project3.R
+++
@@ -1,8 +0,0 @@
-pos.words = scan('C:/Users/Admin/Documents/positive-words.txt', what='character', comment.char=';') #Make sure you edit the location
-neg.words = scan('C:/Users/Admin/Documents/negative-words.txt', what='character', comment.char=';')
-
-
-#Adding words to positive and negative databases
-
-pos.words=c(pos.words, 'Congrats', 'prizes', 'prize', 'thanks', 'thnx', 'Grt', 'gr8', 'plz', 'trending', 'recovering', 'brainstorm', 'leader')
-neg.words = c(neg.words, 'Fight', 'fighting', 'wtf', 'arrest', 'no', 'not')
