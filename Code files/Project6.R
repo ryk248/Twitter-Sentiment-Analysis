99054941bfa08b8ca6b41e5489143757711e81dd
ryk248 <32120690+ryk248@users.noreply.github.com>
2018-01-27 16:28:07 -0500

    Add files via upload

---
+++ Code files/Project6.R
@@ -0,0 +1,5 @@
+slices <- c(sum(table_final$Positive), sum(table_final$Negative))
+labels <- c("Positive", "Negative")
+library(plotrix)
+#pie(slices, labels = labels, col=rainbow(length(labels)), main="Sentiment Analysis")
+pie3D(slices, labels = labels, col=rainbow(length(labels)),explode=0.00, main="Sentiment Analysis")
